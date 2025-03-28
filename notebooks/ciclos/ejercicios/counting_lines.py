28ade29a4cf5a55f1830f63a137a2b0ed2a1c4be
Pablo Salazar Méndez <pablosm@lcg.unam.mx>
2025-03-28 13:04:39 -0600

    Ejercicio 6. Contar secuencias de un archivo FASTA.

---
+++ notebooks/ciclos/ejercicios/counting_lines.py
@@ -0,0 +1,5 @@
+with open("dna_sequences.fa") as seqfa:
+    
+    counts = len([line.strip() for line in seqfa if line.strip().startswith(">")])
+    
+    print(f"\nEl archivo FASTA tiene {counts} secuencias.\n")
