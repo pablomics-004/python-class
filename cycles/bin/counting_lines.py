0406d9c94f6b6579293a8be2369de904d26531aa
Pablo Salazar Méndez <pablosm@lcg.unam.mx>
2025-04-01 14:49:56 -0600

    ACTUALIZACIÓN: Ejercicio 5. Converstir de tsv a fasta

--- cycles/bin/counting_lines.py
+++ cycles/bin/counting_lines.py
@@ -1,9 +1,11 @@
 with open("data/dna_sequences.fa") as seqfa:
     
+    # Conteo de secuencias en el archivo
     counts = len([
         line.strip() 
         for line in seqfa 
         if line.strip().startswith(">")
     ])
     
+    # Resultado para el usuario
     print(f"\nEl archivo FASTA tiene {counts} secuencias.\n")
