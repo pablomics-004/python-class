3ca763447ce2ec91a619121e8d72b3907bd59207
Pablo Salazar Méndez <pablosm@lcg.unam.mx>
2025-04-01 15:04:11 -0600

    ACTUALIZACIÓN: Ejercicio 12. Encontrando las secuencias que tienen un codón de paro.

--- cycles/bin/finding_stop.py
+++ cycles/bin/finding_stop.py
@@ -1,6 +1,13 @@
-usr_seq = [seq.strip() for seq in input("Ingrese varias secuencias de ADN separadas por comas: ").split(",")]
+# Ingreso de las secuencias y retiro de caracteres de escape
+usr_seq = [
+    seq.strip() 
+    for seq in input("Ingrese varias secuencias de ADN separadas por comas: ").split(",")
+]
+
+# Set con los codones de paro
 stop_codons = {"TAA","TAG","TGA"}
 
+# Ubicando secuencias con codones de paro
 seq_stop = {
     seq
     for seq in usr_seq
@@ -10,5 +17,6 @@ seq_stop = {
 
 print(f"\nLas secuencias con al menos un codón de paro son:\n")
 
+# Impresión a pantalla de las secuencias obtenidas
 for seq in seq_stop:
     print(seq,end='\n')
