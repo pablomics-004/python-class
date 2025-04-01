c3e58e24c077c438b1fe72e3e37a19c7d74275a4
Pablo Salazar Méndez <pablosm@lcg.unam.mx>
2025-04-01 14:53:52 -0600

    ACTUALIZACIÓN: Ejercicio 8. De DNA a RNA.

--- cycles/bin/dna_2_rna.py
+++ cycles/bin/dna_2_rna.py
@@ -1,9 +1,12 @@
 with open("data/secuencias_arn.txt","w") as outfile:
 
+    # Ingreso de las secuencias del usuario
     dna_seq = input("Ingrese varias secuencias de ADN separadas por comas: ").split(",")
 
+    # Conversión de DNA a RNA
     rna_seq = [seq.replace("T","U") for seq in dna_seq]
 
+    # Generando el archivo de secuencias de RNA
     for seq in rna_seq:
         print(f"{seq}\n",file=outfile,end='')
     
