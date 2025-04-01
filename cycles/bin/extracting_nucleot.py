a3ec66b373260e2720c570642665246065c8c950
Pablo Salazar Méndez <pablosm@lcg.unam.mx>
2025-04-01 15:01:09 -0600

    ACTUALIZACIÓN: Ejercicio 7. Extracci+on de primeros tres nucleótidos.

--- cycles/bin/extracting_nucleot.py
+++ cycles/bin/extracting_nucleot.py
@@ -1,6 +1,10 @@
 usr_input = input("\nIngrese sus secuencias de DNA separadas por comas: ").split(",")
 
 # Obtención de los primeros tres nucleótidos por secuencia
-seq3 = [seq.strip()[:3] for seq in usr_input]
+seq3 = [
+    seq.strip()[:3] 
+    for seq in usr_input
+]
 
+# Mensaje al usuario con el resultado
 print(f"\nLos primeros tres nucleótidos de cada secuencia son: {seq3}\n")
