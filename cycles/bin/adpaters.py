3d5390924c2c0c18a2d81187f171d0a13ea64bba
Pablo Salazar Méndez <pablosm@lcg.unam.mx>
2025-04-01 14:46:48 -0600

    ACTUALIZACIÓN: Ejercicio 4. Eliminando adaptadores.

--- cycles/bin/adpaters.py
+++ cycles/bin/adpaters.py
@@ -1,5 +1,6 @@
 with open("data/4_input_adapters.txt") as infile, open("results/4_input_no_adapters.txt","w") as outfile:
     
+    # Recorremos el archivo
     for line in infile:
 
         # Retiro de los adaptadores
@@ -8,4 +9,5 @@ with open("data/4_input_adapters.txt") as infile, open("results/4_input_no_adapt
         # Impresión en el nuevo archivo
         print(new_line,file=outfile,end="\n")
     
+    # Mensaje al usuario
     print("\nGeneración del archivo: exitosa.\n")
